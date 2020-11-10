8a6c67047bb3a41fbf1851e6dae22ae3c90230ff
gusttavlang <gusttavlang@gmail.com>
2020-11-10 11:03:33 -0300

    added icon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PySide2.QtWidgets import QApplication
 import sys
-
+from PySide2.QtGui import QIcon
+import os
 
 sys.path.append("src")
 sys.path.append("src/ui")
@@ -8,6 +9,14 @@ sys.path.append("src/ui")
 if __name__ == "__main__":
     from src.mainWindow import MainWindow
     app = QApplication(sys.argv)
+    
+    # setting the icon in applicantion and taskbar:
+    import ctypes
+    myappid = 'pythonw' # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    scriptDir = os.path.dirname(os.path.realpath(__file__))
+    app.setWindowIcon(QIcon(scriptDir + os.path.sep + 'icons/logo.png'))
+    
     mainwindow = MainWindow()
     mainwindow.show()
     sys.exit(app.exec_())
