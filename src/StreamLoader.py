a9e61cf1822f632cbd94de5c02c006df882e3043
gusttavlang <gusttavlang@gmail.com>
2020-11-13 14:22:04 -0300

    save progress

--- src/StreamLoader.py
+++ src/StreamLoader.py
@@ -1,7 +1,39 @@
-from PySide2 import QObject
+from PySide2.QtCore import QObject, Signal
 from pytube import YouTube
 
 
 class StreamLoader(QObject):
+    total_bytes = Signal(int)
+    bytes_remaining = Signal(int)
+    
     def __init__(self, url : str):
+        super().__init__()
         self.yt = YouTube(url)
+        self.resolution = None
+        self.audio_only = None
+        self.item_for_download = 0
+        self.output = ''
+        self.is_first = True
+        
+    def set_resolution(self, resolution):
+        self.resolution = resolution
+        
+    def set_audio_only(self, audio_only):
+        self.audio_only = audio_only
+    
+    def set_item_for_download(self, item_for_download):
+        self.item_for_download = item_for_download
+        
+    def set_output(self, output):
+        self.output = output
+    
+    def start_thread(self):
+        self.yt.streams.filter(resolution=self.resolution, only_audio=self.audio_only)[self.item_for_download].download(self.output)
+        
+    def show_progress_bar(self, stream, chunk, file_handler, bytes_remaining):
+        if (self.is_first):
+            self.total_bytes.emit(bytes_remaining)
+            self.is_first = False
+        self.bytes_remainig.emit(bytes_remaining)
+        
+    
--- src/mainWindow.py
+++ src/mainWindow.py
@@ -1,6 +1,5 @@
-from PySide2.QtWidgets import QMainWindow, QFileDialog
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QProgressDialog
 from src.ui.mainWindow_ui import Ui_MainWindow
-from pytube import YouTube
 from VideoPlayer import VideoPlayer
 from downloadList import DownloadList
 from PySide2.QtCore import QThread
@@ -23,27 +22,46 @@ class MainWindow(QMainWindow):
         self.audio_only = None
         self.ui.le_url.textChanged.connect(self.set_url)
         self.ui.le_output.textChanged.connect(self.set_output)
-        self.ui.rb_144.toggled.connect(self.quality_filter_changed)
-        self.ui.rb_240.toggled.connect(self.quality_filter_changed)
-        self.ui.rb_360.toggled.connect(self.quality_filter_changed)
-        self.ui.rb_480.toggled.connect(self.quality_filter_changed)
-        self.ui.rb_720.toggled.connect(self.quality_filter_changed)
-        self.ui.rb_1080.toggled.connect(self.quality_filter_changed)
-        self.ui.rb_all.toggled.connect(self.quality_filter_changed)
+        self.ui.rb_144.toggled.connect(self.resolution_filter_changed)
+        self.ui.rb_240.toggled.connect(self.resolution_filter_changed)
+        self.ui.rb_360.toggled.connect(self.resolution_filter_changed)
+        self.ui.rb_480.toggled.connect(self.resolution_filter_changed)
+        self.ui.rb_720.toggled.connect(self.resolution_filter_changed)
+        self.ui.rb_1080.toggled.connect(self.resolution_filter_changed)
+        self.ui.rb_all.toggled.connect(self.resolution_filter_changed)
         self.ui.cb_audio.stateChanged.connect(self.audio_olny_changed)
         
-    def download(self):
-        download_thread = QThread()
+        self.download_thread = QThread()        
+        self.progress_dialog = QProgressDialog()   
+        self.progress_dialog.hide()
         
-        yt = YouTube(self.url)
-        download_dialog = DownloadList(yt.streams.filter(resolution=self.resolution, only_audio=self.audio_only))
+    def download(self):        
+        yt = StreamLoader(self.url)
+        download_dialog = DownloadList(yt.yt.streams.filter(resolution=self.resolution, only_audio=self.audio_only))
         download_dialog.exec_()
         
         item_for_download = download_dialog.currentItem
-        yt.register_on_progress_callback(self.show_progress_bar)    
+        yt.yt.register_on_progress_callback(yt.show_progress_bar)    
         
-        yt.moveToThread(download_thread)
-        yt.streams.filter(resolution=self.resolution, only_audio=self.audio_only)[item_for_download].download(self.output)
+        #temp:        
+        yt.total_bytes.connect(self.print_bytes)
+        yt.bytes_remaining.connect(self.print_bytes)
+        
+        
+        yt.set_resolution(self.resolution)
+        yt.set_audio_only(self.audio_only)
+        yt.set_output(self.output)
+        yt.set_item_for_download(item_for_download)
+        
+        
+        yt.moveToThread(self.download_thread)
+        self.download_thread.started.connect(yt.start_thread)         
+        self.progress_dialog.setLabelText('Downloading')
+        yt.total_bytes.connect(self.progress_dialog.setMaximum)
+        yt.bytes_remaining.connect(self.progress_dialog.setValue)
+        self.progress_dialog.show()
+        
+        self.download_thread.start()
         
     def preview(self):
         newWidget = VideoPlayer(self.url)
@@ -64,7 +82,7 @@ class MainWindow(QMainWindow):
     def set_output(self, text : str):
         self.output = text
         
-    def quality_filter_changed(self):
+    def resolution_filter_changed(self):
         if (self.ui.rb_144.isChecked()):
             self.resolution = '144p'
         elif (self.ui.rb_240.isChecked()):
@@ -85,7 +103,19 @@ class MainWindow(QMainWindow):
             self.audio_only = True
         else:
             self.audio_only = None # just cancel this filter          
-            
-    def show_progress_bar(stream, chunk, file_handler, bytes_remaining):
-        print('aqui')
+   
+    '''def show_progress_bar(stream, chunk, file_handler, bytes_remaining):
+        if (is_first[0]):
+            progress_dialog[0] = QProgressDialog()
+            progress_dialog[0].setLabelText('Downloading')
+            total_bytes[0] = bytes_remaining
+            progress_dialog[0].setRange(0, bytes_remaining)
+            progress_dialog[0].show()
+            is_first[0] = False
+            print(is_first[0])
+        progress_dialog[0].setValue(total_bytes[0] - bytes_remaining)'''
+        
+    def print_bytes(self, inteiro):
+        print(inteiro)
+        
     
